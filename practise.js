a94e117052a32e5bf99ee6f6e9c1c80e4e38a9ce
Muhammad Zakaria Abbasi <abbasizakaria467@gmail.com>
2026-04-19 10:51:32 -0700

    Update practise.js

--- practise.js
+++ practise.js
@@ -47,3 +47,77 @@ let Discount=(percent) =>{
       console.log("Your Bill After Discount is "+" "+ billAfterDiscount);
 }
 Discount(50);
+
+
+
+
+//   **********************************************Desrutctrueing in JS...........****************************************
+//*************************************************Arrays Destructring............****************************************
+//********************************without Destructring***************************:
+ let arr=[10,20,30,40,50,60,70,80,90,100];
+ let a = arr[0];
+ let b = arr[4];
+ let c = arr[5];
+ console.log(a,b,c);
+
+let Cars = ["Yaris","Corolla","Civic"];
+ let First = Cars[0];
+ let Second = Cars[1];
+let Third = Cars[2];
+ console.log(First,Second,Third);
+//***************************************************with Destructring*******************************************************:
+let arr=[10,20,30];
+let[a,b,c,d]=arr
+console.log(a,b,c);
+
+let Cars = ["Yaris","Corolla","Civic"];
+let[First,Second, Third] =Cars;
+console.log(First,Third);
+
+// let x=3; 
+let y=5;
+ [x,y]=[y,x]
+ console.log(x,y);
+//**********************************************Object Destructring**************************************** */
+let car={
+    Make:"Toyota",
+    Model:2025,
+    Variant:"Yaris",
+}
+ let {Make:MakeOfCar,Model,Variant} = car;
+console.log(MakeOfCar,Model,Variant);
+
+
+let user={
+name:"Zakaria",
+addres:{
+    city:"Islamabad",
+}
+}
+let {addres:{city}}=user;
+console.log(city);
+//without:
+
+function printUser(user){
+console.log(user.name);
+}
+
+printUser(user);
+
+
+//With:
+function printUser({name}){
+    console.log(name);
+}
+//Real-Life Example (API style)
+let response = {
+  status: 200,
+  data: {
+    username: "zakaria",
+    email: "test@gmail.com"
+  }
+};
+
+let { data: { username, email } } = response;
+
+console.log(username);
